c648838c0b3d6640492499c4d671c0dbb2c95199
russel-oss <russelcsemi@gmail.com>
2026-07-10 11:11:38 +0400

    chore: set web app API URL

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 // Fill after deploying the Apps Script web app (see the foundation plan).
 window.APP_CONFIG = {
   CLIENT_ID: '332855319088-o9oqf9e7b7148v1jqdo2kl1cb8nei1lf.apps.googleusercontent.com',
-  API_URL:   'REPLACE_WITH_API_URL'     // Apps Script web app /exec URL
+  API_URL:   'https://script.google.com/macros/s/AKfycby8FfV-9NWOVB-fFYIi2SzXlysg9Y1jP0SBReBWjr0busXDbiKLZR1Hf7cm9dtqfbx0mQ/exec'
 };
